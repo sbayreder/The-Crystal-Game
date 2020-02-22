83dbd94e4f5d64c5ce84cc0b161cf3d9d838edea
sbayreder <sbayreder@gmail.com>
2020-02-21 19:28:44 -0600

    html and css complete, working on js

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+//arrays
+var wins=[];
+var loss=[];
+
+
+// random number generator
+var random = Math.floor(Math.random() * 120)+19;
+console.log(random)
+
+//function for target number
+
+//$(document).ready(function(){
+  //  $("p").click(function(){
+  //    $(this)//math.floor?  .hide();
+
+
+
+//gem randomizer
+var gems = Math.floor(Math.random()*12)+1;
+console.log (gems)
+
+//function for gem numbers
+//});
