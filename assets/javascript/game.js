3fd939458d501b7ae95af7ecab358f06df01839e
sbayreder <sbayreder@gmail.com>
2020-02-22 08:57:58 -0600

    finished js, tweaking with css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,48 @@
-//arrays
-var wins=[];
-var loss=[];
+$(document).ready(function() {
+  //arrays
+  var wins = [];
+  var loss = [];
+  var counter = 0;
 
+  // random number generator
+  var random = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+  console.log(random);
+  $("#tNum").text(random);
+  
+  //gem randomizer(repeat code for each gem)
+  function randomGenerator(max, min) {
+    var gems = Math.floor(Math.random() * (max - min + 1)) + min;
+    return gems;
+  }
+  var gemsB = randomGenerator(12,1);
+	var gemsG = randomGenerator(12,1);
+	var gemsP = randomGenerator(12,1);
+	var gemsR = randomGenerator(12,1);
+  $("#gemB").on("click", function() {
+    counter += gemsB;
+    console.log(counter);
+    if (counter > random) {
+			alert("You lost!!");
+			random = randomGenerator(120,19);
+			$("#tNum").text(random);
+    } else if (counter == random) {
+      alert("You Win!!");
+    }
+  });
 
-// random number generator
-var random = Math.floor(Math.random() * 120)+19;
-console.log(random)
+  $("#gemG").on("click", function() {
+    counter += gemsG;
+    console.log(counter);
+	});
+	$("#gemP").on("click", function() {
+    counter += gemsP;
+    console.log(counter);
+	});
+	$("#gemR").on("click", function() {
+    counter += gemsR;
+    console.log(counter);
+  });
 
-//function for target number
-
-//$(document).ready(function(){
-  //  $("p").click(function(){
-  //    $(this)//math.floor?  .hide();
-
-
-
-//gem randomizer
-var gems = Math.floor(Math.random()*12)+1;
-console.log (gems)
-
-//function for gem numbers
-//});
+	$('#win').text("Wins :" + wins);
+	$('#loss').text("Loss :" + loss);
+});
